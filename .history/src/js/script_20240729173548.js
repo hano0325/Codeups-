1ac7bf1b708c18168cf56266f437ed1542714899
はの <hamanorinko_13242525@ezweb.ne.jp>
2024-07-29 20:56:13 +0900

    キャンペーンまで

---
+++ .history/src/js/script_20240729173548.js
@@ -0,0 +1,84 @@
+
+jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+  $(".js-hamburger, .js-drawer").click(function() {
+    $(".js-hamburger").toggleClass("is-active");
+    $(".js-drawer").fadeToggle();
+  })
+});
+
+
+      // resizeイベント
+  $(window).resize(function () {
+    if (window.matchMedia("(min-width: 768px)").matches) {
+      closeDrawer();
+    }
+  });
+
+  // 新しい関数: openDrawer
+  function openDrawer() {
+    $(".js-drawer").fadeIn();
+    $(".js-hamburger").addClass("is-active");
+  }
+
+  // 新しい関数: closeDrawer
+  function closeDrawer() {
+    $(".js-drawer").fadeOut();
+    $(".js-hamburger").removeClass("is-active");
+  }
+
+  var topBtn = $('.cycle__button');
+  topBtn.hide();
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 800) {
+      topBtn.fadeIn();
+    } else {
+      topBtn.fadeOut();
+    }
+  });
+
+// スクロールして表示領域に入ったらclass付与
+$(function () {
+  $(".js-slide").on("inview", function () {
+    $(this).addClass("is-inview");
+  });
+});
+
+//要素の取得とスピードの設定
+var box = $('.js-slide'),
+    speed = 700;  
+ 
+//.colorboxの付いた全ての要素に対して下記の処理を行う
+box.each(function(){
+    $(this).append('')
+    var color = $(this).find($('.color')),
+    image = $(this).find('img');
+    var counter = 0;
+ 
+    image.css('opacity','0');
+    color.css('width','0%');
+    //inviewを使って背景色が画面に現れたら処理をする
+    color.on('inview', function(){
+        if(counter == 0){
+　　　　　$(this).delay(200).animate({'width':'100%'},speed,function(){
+                   image.css('opacity','1');
+                   $(this).css({'left':'0' , 'right':'auto'});
+                   $(this).animate({'width':'0%'},speed);
+                })
+            counter = 1;
+          }
+     });
+});
+
+
+
+
+  const swiper = new Swiper(".swiper", {
+    loop: true,
+    effect: "fade",
+    speed: 3000,
+    allowTouchMove: false,
+    autoplay: {
+      delay: 3000,
+    },
+  });
---
+++ .history/src/js/script_20240729182604.js
@@ -0,0 +1,61 @@
+
+jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+  $(".js-hamburger, .js-drawer").click(function() {
+    $(".js-hamburger").toggleClass("is-active");
+    $(".js-drawer").fadeToggle();
+  })
+});
+
+
+      // resizeイベント
+  $(window).resize(function () {
+    if (window.matchMedia("(min-width: 768px)").matches) {
+      closeDrawer();
+    }
+  });
+
+  // 新しい関数: openDrawer
+  function openDrawer() {
+    $(".js-drawer").fadeIn();
+    $(".js-hamburger").addClass("is-active");
+  }
+
+  // 新しい関数: closeDrawer
+  function closeDrawer() {
+    $(".js-drawer").fadeOut();
+    $(".js-hamburger").removeClass("is-active");
+  }
+
+  var topBtn = $('.cycle__button');
+  topBtn.hide();
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 800) {
+      topBtn.fadeIn();
+    } else {
+      topBtn.fadeOut();
+    }
+  });
+
+  $(function () {
+    $(window).on('scroll', function() {
+      $('.js-slide').each(function() {
+        if ($(this).offset().top < $(window).scrollTop() + $(window).height() - 100) {
+          $(this).addClass('is-inview');
+        }
+      });
+    });
+  });
+  
+
+
+
+  const swiper = new Swiper(".swiper", {
+    loop: true,
+    effect: "fade",
+    speed: 3000,
+    allowTouchMove: false,
+    autoplay: {
+      delay: 3000,
+    },
+  });
