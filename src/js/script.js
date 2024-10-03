bc08a0b5a370a56e7ec58fb45b6eb7178435541e
hano0325 <hano.13242525@gmail.com>
2024-10-03 18:05:36 +0900

    infotmation下層からblogの下層まで

--- src/js/script.js
+++ src/js/script.js
@@ -158,4 +158,18 @@ box.each(function(){
       });
     });
   });
-  
+
+    // コース画像モーダル表示イベント
+    $(".gallery-list__item img").click(function () {
+      // まず、クリックした画像の HTML(<img>タグ全体)を#frayDisplay内にコピー
+      $("#grayDisplay").html($(this).prop("outerHTML"));
+      //そして、fadeInで表示する。
+      $("#grayDisplay").fadeIn(200);
+      return false;
+    });
+
+    $("#grayDisplay").click(function () {
+      // 非表示にする
+      $("#grayDisplay").fadeOut(200);
+      return false;
+    });
